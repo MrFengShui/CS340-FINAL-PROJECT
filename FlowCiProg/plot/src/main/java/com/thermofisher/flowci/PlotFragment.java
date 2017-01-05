404b2eab4e174f61ef114783173dc1062284c720
MrFengShui <luansongjian@gmail.com>
2017-01-05 14:11:06 -0800

    tabbed panel implement

---
+++ FlowCiProg/plot/src/main/java/com/thermofisher/flowci/PlotFragment.java
@@ -0,0 +1,24 @@
+package com.thermofisher.flowci;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class PlotFragment extends Fragment {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.layout_plot_fragment, container, false);
+        TextView tabContent = (TextView) view.findViewById(R.id.fragment_text_view);
+        tabContent.setText(this.getTag() + " Content");
+        return view;
+    }
+
+}
